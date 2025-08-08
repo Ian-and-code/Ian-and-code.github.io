466b29448be7b444f7d114deb3b3a453b0fa7c27
Ian <supergachaworld4@gmail.com>
2025-08-08 20:43:40 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function cambiarMensaje() {
     const mensajeElemento = document.getElementById('mensaje');
     mensajeElemento.textContent = '¡Has cambiado el mensaje con JavaScript!';
 }
-function cambiarMensaje() {
+function cambiarMensaje2() {
     const mensajeElemento = document.getElementById('mesage');
     mensajeElemento.textContent = '¡Hola, Universo!';
 }
