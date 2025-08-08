31a777afaf3b3bb4bf22733e3c1d33fb3e6a05db
Ian <supergachaworld4@gmail.com>
2025-08-08 20:39:27 -0300

    Crear script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript para añadir interactividad
+function cambiarMensaje() {
+    const mensajeElemento = document.getElementById('mensaje');
+    mensajeElemento.textContent = '¡Has cambiado el mensaje con JavaScript!';
+}
+function cambiarMensaje() {
+    const mensajeElemento = document.getElementById('mesage');
+    mensajeElemento.textContent = '¡Hola, Universo!';
+}
