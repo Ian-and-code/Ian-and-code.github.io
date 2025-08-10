065fc3b51079b83328b6ee143d23242c242d1e38
Ian <supergachaworld4@gmail.com>
2025-08-09 21:59:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function go_to(){
-    window.location.href= "${html}.html"
+function go_to1(){
+    window.location.href= "redes/redes.html"
+}
+function go_to2(){
+    window.location.href= "redesm/redesm.html"
 }
