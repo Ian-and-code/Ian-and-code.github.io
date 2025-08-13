1e1e14e70a0baffbfe43a56630038325cf44cd35
Ian <supergachaworld4@gmail.com>
2025-08-13 07:53:52 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const app = document.querySelector('#app')
-const md = fetch('/Index.md').then((response) => response.text()).then((text) => {
-    const converter = new showdown.Converter()
-    const html = converter.makeHtml(text)
-    app.innerHTML = html
-  })
