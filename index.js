32256dc1d291815ed498108f91c86741509fb3bd
Ian <supergachaworld4@gmail.com>
2025-08-13 07:44:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = document.querySelector('#app')
-const md = fetch('Index.md').then((response) => response.text()).then((text) => {
+const md = fetch('https://github.com/Ian-and-code/Ian-and-code.github.io/blob/main/index.js').then((response) => response.text()).then((text) => {
     const converter = new showdown.Converter()
     const html = converter.makeHtml(text)
     app.innerHTML = html
