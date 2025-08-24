953993f9d30a8d2a3e9932256da427f47142e615
Ian <supergachaworld4@gmail.com>
2025-08-23 21:29:05 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ class MyFooter extends HTMLElement {
         <h1>pags</h1>
         <ul>
           <li> <a href="index.html">principal</a></li>
-          <li> <a href="Estudios.html">estudios</a></li>
+          <li> <a href="estudios.html">estudios</a></li>
         </ul>
       </footer>
     `;
