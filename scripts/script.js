8464bc10b5c6d1342fede306ec2fd8d2998d60c8
Ian <supergachaworld4@gmail.com>
2025-08-23 21:01:00 -0300

    Actualizar script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-console.log("eres moderador o hacker")
+class ManzDevHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = /* html */ `
+      <header>
+        <h1>Manz.dev</h1>
+        <img src="https://manz.dev/manz-logo.svg" alt="Manz.dev">
+      </header>
+    `;
+  }
+}
+
+customElements.define("manzdev-header", ManzDevHeader);
